29924da7ade51139d26c0ed5f08047b94227fcbc
abrar zahin <abrarzahin@yahoo.com>
2020-06-12 16:46:38 -0400

    sametree

---
+++ sametree.java
@@ -0,0 +1,30 @@
+public class TreeNode{
+    int val;
+    TreeNode left;
+    TreNode right;
+    TreeNode() {}
+    TreeNode (int val ) { this.val = val;}
+    TreeNode( int val , TreeNode left, TreeNode right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public boolean inSameTree( TreeNode p, TreeNode q){
+        if( p == null || q == null){
+            return true;
+        }
+
+        if( p!= null || q != null){
+            if(p.val == q.val && isSameTree(p.left, q.left)
+                              && isSameTree(p.right, q.right)){
+                                  return true;
+                              }
+        }
+
+        return false;
+    }
+
+}
