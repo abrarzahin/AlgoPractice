19f37d508c9f1c711548ee860240bfbfa862717d
abrar zahin <abrarzahin@yahoo.com>
2020-06-26 16:51:47 -0400

    word-search

---
+++ word-search.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        
+        int m = board.length, n = board[0].length;
+        
+        for( int i =0; i < m ; i++){
+            for( int j =0; j < n ; j++){
+                
+                if( word.charAt(0) == board[i][j] && helper(board, i, j, word, 0)){
+                    return true;
+                }
+                
+            }
+        }
+        
+        return false;
+    }
+    
+    private boolean helper( char [][] board, int row, int col, 
+                           String word, int count){
+        
+        if( count == word.length()) return true;
+        
+        if( row < 0 || col < 0 || row >= board.length ||
+          col >= board[0].length || board[row][col] != word.charAt(count)){ 
+            return false;
+        }
+        
+       
+        char temp = board[row][col];
+        board[row][col] = ' ';
+        
+        
+        boolean found = helper(board, row+1, col, word, count+1) ||
+          helper(board, row-1, col, word, count+1) ||
+          helper(board, row, col-1, word, count+1) ||
+          helper(board, row, col+1, word, count+1);
+        
+        board[row][col] = temp;
+        
+        return found;
+        
+        
+        
+    }
+    
+    
+}
