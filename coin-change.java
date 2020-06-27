12db6c5e1005aeddfb9e87e1bf5be31e5b308b4c
abrar zahin <abrarzahin@yahoo.com>
2020-06-27 18:36:34 -0400

    coin-change

---
+++ coin-change.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        
+        Arrays.sort(coins);
+        int max = amount+1;
+        
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp, max);
+        
+        dp[0]=0;
+        for( int i = 1; i<= amount; i++){
+            for(int j=0; j < coins.length; j++){
+                if(coins[j]<=i){
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+                else {
+                    break;
+                }
+            }
+        }
+        
+        return dp[amount] > amount ? -1 : dp[amount];
+        
+    }
+}
