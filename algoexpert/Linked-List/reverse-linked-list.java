573af236212d02153ea358c84c933354c5b8283c
abrar zahin <abrarzahin@yahoo.com>
2020-08-21 11:51:50 -0400

    reverse linked list

---
+++ algoexpert/Linked-List/reverse-linked-list.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Program {
+  public static LinkedList reverseLinkedList(LinkedList head) {
+    LinkedList p1= null;
+		LinkedList p2 = head;
+		
+		while(p2 != null){
+			LinkedList p3 = p2.next;
+			p2.next = p1;
+			p1 = p2;
+			p2 = p3;
+		}
+		return p1;
+  }
+
+  static class LinkedList {
+    int value;
+    LinkedList next = null;
+
+    public LinkedList(int value) {
+      this.value = value;
+    }
+  }
+}
+
