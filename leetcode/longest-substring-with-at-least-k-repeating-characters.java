1c58aece12d02ae86b1712944ef3a08667ebbbc5
mosume123 <mosumebahar@gmail.com>
2021-08-04 13:03:11 -0400

    longest subs updated

--- leetcode/longest-substring-with-at-least-k-repeating-characters.java
+++ leetcode/longest-substring-with-at-least-k-repeating-characters.java
@@ -1,45 +1,35 @@
+import java.util.HashSet;
+
 class Solution {
-    public int longestSubstring(String s, int k) {
-        
-        char[] str= s.toCharArray();
-        
-        return helper(str,0,s.length(),k);
-    }
-    
-    
-    private int helper(char[] str, int start, int end, int k){
-        
-        if(end-start < k) return 0;// no need to go for length shorter 
-then k.
-        
-        int[] count = new int[26];
-        
-        for( int i=start; i < end; i++){
-            int index = str[i]-'a';
-            
-            count[index]++;
-        }
-        
-        
-        for( int i=0; i < 26; i++){
-            
-            if( count[i] < k && count[i] > 0){// count[i]=0 --> i+'a' 
-does not exist, thats why lower limit
-                for( int j=start; j < end; j++){
-                    if( str[j]==i+'a'){
-                        int left = helper(str, start, j , k);
-                        int right = helper(str, j+1, end, k);
-                        return Math.max(left,right);
-                    }
-                }
+    public int lengthOfLongestSubstring(String s) {
+
+
+        int maxLength = 0;
+
+        HashSet<Character> set = new HashSet<>();
+
+        int i = 0;
+        int j = 0;
+
+        while ( j< s.length){
+
+            if( !set.contains(s.charAt(j))){
+                set.add(s.charAt(j));
+                j++;
+                maxLength = Math.max(maxLength, j-i);
             }
-            
+
+            else{
+                set.remove((s.charAt(i)));
+                i++;
+            }
+
+
         }
         
         
+        return maxLength;
         
-        
-        return end-start;
         
     }
 }
