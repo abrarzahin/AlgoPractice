85b4be5ae0900840da9ed003ff0a7ed50ce8faef
abrarzahin <abrarzahin@yahoo.com>
2021-08-10 11:16:58 -0400

    roman to integer

--- leetcode/roman-to-integer.java
+++ leetcode/roman-to-integer.java
@@ -1,10 +1,9 @@
 class Solution {
     public int romanToInt(String s) {
-            
+        
         if( s == null || s.length() == 0){
             return 0;
         }
-        
         
         Map<Character, Integer> romanValues = new HashMap<>();
         
@@ -12,22 +11,23 @@ class Solution {
         romanValues.put('V', 5);
         romanValues.put('X', 10);
         romanValues.put('L', 50);
-        romanValues.put('C',100);
+        romanValues.put('C', 100);
         romanValues.put('D', 500);
         romanValues.put('M', 1000);
         
         int length = s.length();
         
-        int result = romanValues.get(s.charAt(length - 1));
+        int result = romanValues.get(s.charAt(length-1));
         
-        for(int i = length-2 ; i >= 0; i--){
+        for( int i = length-2; i >=0; i--){
             
-            if(romanValues.get(s.charAt(i)) >= romanValues.get(s.charAt(i+1)))
-            {
-                result += romanValues.get(s.charAt(i));
-            }
-            else
-            {
+            if( romanValues.get(s.charAt(i)) >= 
+romanValues.get(s.charAt(i+1))){
+                
+                    result += romanValues.get(s.charAt(i));
+                
+            } else {
+                
                 result -= romanValues.get(s.charAt(i));
             }
             
