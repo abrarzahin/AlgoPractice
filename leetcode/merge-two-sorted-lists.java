46a63544eb3f2556fc252f51def98d71ad5c46b0
abrarzahin <abrarzahin@yahoo.com>
2021-08-13 14:42:25 -0400

    merge two sorted lists

--- leetcode/merge-two-sorted-lists.java
+++ leetcode/merge-two-sorted-lists.java
@@ -5,34 +5,35 @@
  *     ListNode next;
  *     ListNode() {}
  *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = 
+next; }
  * }
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        ListNode l3= dummy;
         
-        while(l1 != null && l2 !=null){
-            if(l1.val <= l2.val){
-                l3.next=l1;
-                l1=l1.next;
-            }
-            else
-            {
-                l3.next=l2;
-                l2=l2.next;
-            }
+        ListNode dummy = new ListNode(0);
+        ListNode l3 = dummy;
+        
+        while( l1 != null && l2 != null){
             
-            l3=l3.next;
+            if(l1.val <= l2.val){
+                l3.next = l1;
+                l1=l1.next;
+            } else {
+                l3.next = l2;
+                l2 = l2.next;
+            }
+            l3 = l3.next;
         }
+        
         
         if( l1 != null){
-            l3.next=l1;
+            l3.next = l1;
         }
         
-        if(l2 != null){
-            l3.next= l2;
+        if( l2 != null){
+            l3.next = l2;
         }
         
         return dummy.next;
