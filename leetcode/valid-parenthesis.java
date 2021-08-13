89030b4eec8d0f07b9e636064a3c857b8d82057d
abrarzahin <abrarzahin@yahoo.com>
2021-08-13 14:11:06 -0400

    valid parenthesis

--- leetcode/valid-parenthesis.java
+++ leetcode/valid-parenthesis.java
@@ -3,43 +3,39 @@ class Solution {
         
         Stack<Character> stack = new Stack<Character>();
         
-        boolean isValid= false;
+        boolean isValid = false;
         
-        for(int i=0; i<s.length();i++){
+        for( int i=0; i < s.length(); i++){
+            
             
             char ch = s.charAt(i);
             
-            if(ch == '(' || ch == '{' || ch =='['){
+            if( ch == '(' || ch == '{' || ch == '['){
                 stack.push(ch);
-            }
-            else 
-            {
-                if( !stack.isEmpty() && isPairParenthesis(stack.peek(), ch)){
-                    stack.pop();
-                }
-                
-                else
-                {
-                    return false;
-                }
+            } else {
                     
-                
-            }    
+                    if( !stack.isEmpty() && 
+isPairParenthesis(stack.peek(), ch)){
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+            }
+            
             
         }
-        
         
         return stack.isEmpty();
         
     }
     
     
-    private boolean isPairParenthesis(char c1, char c2){
-        return (c1 =='(' && c2==')')
-            || (c1=='{' && c2=='}')
-            || (c1=='[' && c2==']');
+    
+    private boolean isPairParenthesis( char c1, char c2){
+        return ( c1 == '(' && c2 == ')'
+               || c1 == '{' && c2 =='}'
+                || c1 == '[' && c2 == ']'
+               
+               );
     }
-    
-    
-    
 }
