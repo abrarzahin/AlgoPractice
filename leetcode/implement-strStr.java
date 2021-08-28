4442e326099d4314b610e8494ef4c871f19a8023
abrarzahin <abrarzahin@yahoo.com>
2021-08-28 14:07:23 -0400

    implement strStr

--- leetcode/implement-strStr.java
+++ leetcode/implement-strStr.java
@@ -1,24 +1,26 @@
 class Solution {
     public int strStr(String haystack, String needle) {
         
-        if(haystack ==null || needle == null){
+        if(haystack == null || needle == null){
             return -1;
         }
         
-        for(int i=0; i<haystack.length()-needle.length()+1;i++){
+        for( int i=0; i < haystack.length()-needle.length()+1;i++){
             
+          
             int j;
             
-            for( j=0; j< needle.length(); j++){
-                if (haystack.charAt(i+j) !=  needle.charAt(j)){
+            for( j=0; j < needle.length(); j++){
+                
+                if( haystack.charAt(i+j) != needle.charAt(j)){
                     break;
                 }
+                
             }
             
             if( j==needle.length()){
                 return i;
             }
-            
             
         }
         
