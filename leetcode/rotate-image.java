cbe49b6235c3754a6bb818bc64aac4d3364e235e
abrarzahin <abrarzahin@yahoo.com>
2021-08-30 14:04:25 -0400

    rotate image

--- leetcode/rotate-image.java
+++ leetcode/rotate-image.java
@@ -1,20 +1,34 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int N = matrix.length;
         
-        for(int i =0; i <N; i++){
-            for(int j=i; j<N; j++){
-                int temp= matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+        transpose(matrix);
+        reflect(matrix);
+        
+    }
+    
+    
+    public void transpose(int[][] matrix){
+        int n = matrix.length;
+        
+        for( int i=0; i <n; i++){
+            for( int j=i; j < n; j++){
+                int temp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = temp;
             }
         }
         
-        for( int i=0; i <N; i++){
-            for(int j =0; j <(N/2); j++){
-                int temp= matrix[i][j];
-                matrix[i][j] = matrix[i][N-1-j];
-                matrix[i][N-1-j] = temp;
+    }
+    
+    
+    public void reflect(int[][] matrix){
+        int n= matrix.length;
+        
+        for( int i=0; i <n; i++){
+            for( int j=0; j < n/2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = temp;
             }
         }
         
